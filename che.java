4ec8f513db071eab9ea22df74483e7e68add8ec2
chelei <15754886027@163.com>
2016-07-13 09:32:42 +0800

    第二次提交

--- che.java
+++ che.java
@@ -3,5 +3,13 @@ public class che
 	public static void main(String [] args)
 	{
 		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
+		System.out.println("hello world!");
 	}
 }
