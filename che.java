d4a99e361cbbacfbd45a09c5744ecdaf51516cda
15754886027 <15754886027@163.com>
2016-07-14 11:08:48 +0800

    测试提交

--- che.java
+++ che.java
@@ -2,6 +2,10 @@ public class che
 {
   public static void main(String [] args)
   {
+    for(int i=0;i<10;i++)
+    {
+        System.out.pritnln("hello world!");
+    }
     System.out.pritnln("hello world!");
   }
 }
